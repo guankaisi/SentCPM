4ec14ca5ec6406f1319d84ac0844669554fedb20
fuan-cpu <2416809043@qq.com>
2023-07-05 17:26:19 +0800

    add code

---
+++ SentCPM/cpmcl_model.py
@@ -0,0 +1,37 @@
+
+import torch.distributed as dist
+import torch.nn as nn
+import torch
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+class MLPLayer(nn.Module):
+    """
+    Head for getting sentence representations over RoBERTa/BERT's CLS representation.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.activation = nn.Tanh()
+
+    def forward(self, features, **kwargs):
+        x = self.dense(features)
+        x = self.activation(x)
+
+        return x
+
+
+class Similarity(nn.Module):
+    """
+    Dot product or cosine similarity
+    """
+
+    def __init__(self, temp):
+        super().__init__()
+        self.temp = temp
+        self.cos = nn.CosineSimilarity(dim=-1)
+
+    def forward(self, x, y):
+        return self.cos(x, y) / self.temp
+
+
